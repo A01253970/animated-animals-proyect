0481547ab6ba0179ecfe52d2e683e08c6bc5282b
A01253970 <110990980+A01253970@users.noreply.github.com>
2022-09-07 15:45:31 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Duck)
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # . .
+            . # # # #
+            . # # # .
+            `)
+    }
+})
